a559c84d69b0570f08285dc7ea24ad2ffff6d177
VIVEKj24 <vjvivekjain2406@gmail.com>
2025-01-19 20:01:13 +0530

    The main Shoe and shoe change on click

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,6 +1,16 @@
-const ShoeCard = ({imgUrl , changeBigShoe , BigShoeImage}) => {
+const ShoeCard = ({shoe , changeBigShoe , bigShoeImage}) => {
+  //imgUrl -> The original Shoe , changeBigshoes is a setter function and bigShoesImage is empty
+  const handleClick = ()=>{
+    if(bigShoeImage!==shoe.bigShoe){
+      changeBigShoe(shoe.bigShoe)
+    }
+  }
   return (
- <div className="border-2 rounded-xl"></div>   
+ <div className={`border-2 rounded-xl ${bigShoeImage==shoe.bigShoe ? 'border-coral-red':'border-transparent'} cursor-pointer max-sm:flex-1`}
+ onClick={handleClick}
+ ><div>
+  <img src={shoe.thumbnail}/> </div>
+ </div>   
 )}
-
+ 
 export default ShoeCard
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,13 +3,15 @@ import {arrowRight} from '../../assets/icons/index'
 import { shoes, statistics } from "../constants"
 import { bigShoe1 } from "../../assets/images/index"
 import ShoeCard from "../components/ShoeCard"
+import { useState } from "react"
 const Hero = () => {
+  const [bigShoeImg,setBigShoeImg] = useState(bigShoe1)
   return (
     <section id="home" className=" w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container  ">
       <div className="relative xl:w-2/5 flex flex-col justify-center w-full items-start max-xl:padding-x mt-28">
         <p>Our Summer Collection</p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[83]">
-          <span className="max-xl:bg-white xl:whitespace-nowrap font-bold relative z-10 pr-10">The  New Arrival</span>
+          <span className="xl:bg-white xl:whitespace-nowrap font-bold relative z-10 pr-10">The  New Arrival</span>
           <br/>
           <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
@@ -17,7 +19,7 @@ const Hero = () => {
         <Button label ="Shop now"
         iconUrl={arrowRight}/>
 
-        <div className="flex flex-row justify-start    items-start flex-wrap w-full mt-20 gap-16 ">
+        <div className="flex flex-row justify-start items-start flex-wrap w-full mt-20 gap-16 ">
           {statistics.map((stat,index)=>(
             <div key={stat.value}>
               <p className="font-palanquin text-4xl font-bold">{stat.value}</p>
@@ -28,11 +30,11 @@ const Hero = () => {
       </div>
 
       <div className="relative flex-1 flex justify-center  items-center xl:min-h-screen max:xl:py-40  bg-primary bg-hero bg-cover bg-center">
-        <img src={bigShoe1} width={610} height={500} alt="" className="object-contain relative z-10 x" />
-        <div>
+        <img src={bigShoeImg} width={610} height={500} alt="" className="object-contain relative z-10 x" />
+        <div >
           {shoes.map((shoe=>(
-            <ShoeCard imgUrl={shoe}
-            changeBigShoe={()=>{}}/>
+            <ShoeCard shoe={shoe} changeBigShoe={(shoe)=>{setBigShoeImg(shoe)}} bigShoeImage={bigShoeImg} />
+         
           )))}
       </div>
       </div>
