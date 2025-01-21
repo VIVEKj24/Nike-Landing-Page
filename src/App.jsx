d87137d29dfde456401c11c23a04f6cbe55b92e0
VIVEKj24 <vjvivekjain2406@gmail.com>
2025-01-22 01:42:52 +0530

    footer fixing

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import Subscribe from "./sections/Subscribe";
 import SuperQuality from "./sections/SuperQuality";
 import SpecialOffer from "./sections/SpecialOffer";
 import Services from "./sections/Services";
-
-import Footer from "./sections/Footer";
 import PopularProducts from "./sections/PopularProducts";
 import Nav from "./components/Nav";
 const App = () => {
@@ -36,9 +34,7 @@ const App = () => {
       <section className='padding-x sm:py-32 py-16 w-full'>
         <Subscribe />
       </section>
-      <section className=' bg-black padding-x padding-t pb-8'>
-        <Footer />
-      </section>
+      
     </main>
   );
 };
